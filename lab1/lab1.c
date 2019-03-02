cb5db5edff03967d36a1a7d71e46809de56b6a39
Vitaly <47782238+MyCoRIIIiK@users.noreply.github.com>
2019-03-02 12:43:27 +0300

    Update lab1.c

--- lab1/lab1.c
+++ lab1/lab1.c
@@ -1,36 +1,40 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
-///////////////////
-///////////////////   вариант 24
-///////////////////
-int fact(int x);
+int coef(int i, int j);
 
 int main()
 {
-	 int n, sum = 0, proizv=1;
-	  int i, j,s;
+	int sum = 0, proizv = 1;
+	int i, j, s,n;
 	printf("Input n:");
 	scanf_s("%d", &n);
 
 	for (i = 1; i <= n; i++)
 	{
-		proizv = 1;		
+		proizv = 1;
 		for (j = 1; j <= i; j++)
 		{
-			proizv *= (fact(j + i) / fact(i));
+			proizv *= coef(j,i);
 		}
 		sum += proizv;
 	}
 
 	printf("sum=%d", sum);
-
-	scanf_s("%d", &n);
+	scanf("%d", &n);	
 	return 0;
 }
 
 
-int fact(int x)
+int coef(int j, int i)
 {
-	if (x <= 1) return x;
-	else  return x * fact(x - 1);
+	int n = i+1;
+
+	for (int count = n+1; count <= i+j; count++)
+	{
+		n *= count;
+	}
+
+	return n;
+
 }
