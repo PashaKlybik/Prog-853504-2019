8fc3e2cfa6a8be4d228092b6f582d3586973d46d
NotNikita <47776039+NotNikita@users.noreply.github.com>
2019-03-05 13:13:35 +0300

    Update lab1.c

--- lab1.c
+++ lab1.c
@@ -1,7 +1,6 @@
-#include <conio.h>
 #include <stdio.h>
 
-void help(int * some, int dec, char L);
+void func(int * some, int dec, char L);
 
 int main()
 {
@@ -9,31 +8,27 @@ int main()
 	do {
 		printf("\n\nenter number:\n");
 		scanf_s("%d", &num);
-		help(&num, 10000, 'X'); // X большое означает 10тыс
-		help(&num, 5000, 'ṽ');
-		help(&num, 1000, 'M');
-		help(&num, 500, 'D');
-		help(&num, 100, 'C');
-		help(&num, 50, 'L');
-		help(&num, 10, 'x'); // х малое означает 10
-		help(&num, 5, 'V');
-		help(&num, 1, 'I');
+		func(&num, 10000, 'X'); // X большое означает 10тыс
+		func(&num, 5000, 'ṽ');
+		func(&num, 1000, 'M');
+		func(&num, 500, 'D');
+		func(&num, 100, 'C');
+		func(&num, 50, 'L');
+		func(&num, 10, 'x'); // х малое означает 10
+		func(&num, 5, 'V');
+		func(&num, 1, 'I');
 	} while (1);
-	_getch();
 	return 0;
 }
 
-void help(int * some, int dec, char L)
+void func(int * some, int dec, char L)
 {
 	int num = *some;
-	//printf("\nnum=%d  ost=%d", num, dec);
 	int ost = num / dec;
-	//printf("\nnum=%d  ost=%d", num, ost);
 	if (ost > 0)
 	{
 		for (int i = 0; i < ost; i++) printf("%c",L);
 	}
 	num -= ost * dec;
 	*some = num;
-	
 }
