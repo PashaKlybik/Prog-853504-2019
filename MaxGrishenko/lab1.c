7d7da7a8df14ad24a6f0e3682906c0f944141607
MaxGrishenko <48172794+MaxGrishenko@users.noreply.github.com>
2019-03-05 13:02:07 +0300

    Update lab1.c

--- MaxGrishenko/lab1.c
+++ MaxGrishenko/lab1.c
@@ -4,22 +4,22 @@
 
 int main()
 {
-	int k,Result;
-	printf ("Enter sequence 'k' : ");
-	scanf("%d",&k);
-	if ((k > 0) && (k < 2701)) {        //Проверка допустимых знач. k-ой послед.
+	int Sequence,Result;
+	printf ("Enter sequence : ");
+	scanf("%d",&Sequence);
+	if ((Sequence > 0) && (Sequence < 2701)) {        //Проверка допустимых знач. k-ой послед.
 		int Number = 100,Counter = 0;
-		if (k / 3 >= 1) {			   	//Обработка исключения для 3-x знач.ч.
-			while (Counter < k / 3){	//Ищем нужное трехзначное число
+		if (Sequence / 3 >= 1) {			   	//Обработка исключения для 3-x знач.ч.
+			while (Counter < Sequence / 3){	//Ищем нужное трехзначное число
 				Number++;
 				Counter++;
 			}
-			if (k % 3 == 0) {           //Обработка для перехода на другое ч.
+			if (Sequence % 3 == 0) {           //Обработка для перехода на другое ч.
 				Number--;
 			}
 		}
 		printf("Number of threes : %d\n",Number);
-		int Way = k % 3;                //Определяем ветку
+		int Way = Sequence % 3;                //Определяем ветку
 		switch(Way){                    //Изымаем нужную цифру из 3-х знач. ч.
 			case 1:printf("Result : %d", Number / 10 / 10);break;
 			case 2:printf("Result : %d", Number / 10 % 10);break;
