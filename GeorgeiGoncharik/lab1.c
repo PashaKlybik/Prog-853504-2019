68c5516c20f7ca42ca4f744ca984f828903957d3
Georgei Goncharik <georgii.goncharik@gmail.com>
2019-03-05 13:19:37 +0300

    Update lab1.c

--- GeorgeiGoncharik/lab1.c
+++ GeorgeiGoncharik/lab1.c
@@ -1,43 +1,23 @@
 #include <stdio.h> 
 #include <conio.h>
-#include <string.h>
+#include <math.h>
 /*
 Variant-6
 */
-int ConvertCharToInt(char letter)
+int main() 
 {
-	switch (letter)
-	{
-	case '1':return 1;
-	case '2':return 2;
-	case '3':return 3;
-	case '4':return 4;
-	case '5':return 5;
-	case '6':return 6;
-	case '7':return 7;
-	case '8':return 8;
-	case '9':return 9;
-	case '0':return 0;
-	}
-}
-
-int main() {
-	char fraction[15];
+	float fraction;
+	int sumOfFirstThreeDigits = 0;
 
 	printf("Enter fraction number: ");
-	gets_s(fraction, 14);
-	int lengthOfFraction = strlen(fraction);
-	for (int i = 0; i < lengthOfFraction; i++)
+	scanf_s("%f", &fraction);
+	fraction = (fabs(fraction) - (int)(fabs(fraction))) * 1000;
+	int threeDigits = fraction;
+	for (int i = 0; i < 3; i++)
 	{
-		if (fraction[i] == '.')
-		{
-			int sumOfFirstThreeDigits = 0;
-			for (int j = i + 1; j <= i + 3; j++)
-			{
-				sumOfFirstThreeDigits += ConvertCharToInt(fraction[j]);
-			}
-			printf("the sum of the first three digits after the decimal point: %d\n", sumOfFirstThreeDigits);
-		}
+		sumOfFirstThreeDigits += threeDigits % 10;
+		threeDigits /= 10;
 	}
+	printf("Sum of the first three digits: %d", sumOfFirstThreeDigits);
 	return 0;
 }
