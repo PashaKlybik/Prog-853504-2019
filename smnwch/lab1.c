cc2fa725779bf5f8deec882d544d9c316860d68e
Катя Симанович <48224454+smnwch@users.noreply.github.com>
2019-03-19 12:57:05 +0300

    Update lab1.c

--- smnwch/lab1.c
+++ smnwch/lab1.c
@@ -1,25 +1,30 @@
 #include <stdio.h>
-#include <conio.h>
-//variant28
 int main()
-{   int k, i, r, j=1, c=1;
-	printf("input k: ");
-	scanf_s("%d",&k);
-	if (k==0) {
-	   printf("0");
-	} else	if (k==1||k==2)
-	  {
-		printf("1");
-	  } else if (k>2)
-		   { for ( i = 3; i <= k; i++) {
-				  r=j+c;
-				  j=c;
-				  c=r;
-				  }
-			printf("%d",r);
-			}
-			else { printf("error");}
-
-	 getch();
-	 return 0;
+{
+	int k, n, n1 =1, n2 = 1, result;
+	int digits = 2;    //счетчик цифр
+	printf("k: ");
+	scanf_s("%d", &k);
+	while (digits < k)
+	{
+		n = n1 + n2;		//считаем новое число фибоначчи
+		n1 = n2;
+		n2 = n;
+		while (n)
+		{
+			digits++; //считаем кол-во цифр в числе n
+			n/=10;
+		}
+	}
+	if (k >=3) {
+		//находим нужную цифру в числе n2
+		while (digits > k)
+		{
+			digits--;
+			n2 /= 10;
+		}
+		result = n2%10;
+		printf("digit: %d", result);
+	} else {printf("%d", 1); }
+	return 0;
 }
