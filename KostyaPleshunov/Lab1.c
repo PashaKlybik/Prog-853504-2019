5ba746f63acb6f3b6002e9b89b73c0f4e124ea1d
KosP1 <48713029+KosP1@users.noreply.github.com>
2019-03-19 12:25:36 +0300

    Update Lab1.c

--- KostyaPleshunov/Lab1.c
+++ KostyaPleshunov/Lab1.c
@@ -1,19 +1,18 @@
 #include "pch.h"
 #include <stdio.h>
 #include <cstdlib>
-
 int main()
 {
     int i = 0;
-    for (int z = 1; ; ++z)
-		  for (int x = 1; x <= z; ++x)
-			  for (int y = x; y <= z; ++y)
-				  if (x*x + y * y == z * z)
-				{
-          printf("(%i,%i,%i)\n", x, y, z);
-					if (++i == 10)
-					goto t;
-				}
+    for (int m = 1; ; ++m)
+	for (int k = 1; k <= m; ++k)
+	    for (int l = k; l <= m; ++l)
+	     if (k * k + l * l == m * m)
+	{
+             printf("(%i,%i,%i)\n", x, y, z);
+	     if (++i == 10)
+	     goto t;
+        }
     t:
     system("pause");
 return 0;
