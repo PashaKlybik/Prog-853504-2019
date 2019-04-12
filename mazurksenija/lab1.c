1a71d8d48ce1c0ef71aea3b56199d136dc7936e2
mazurksenija <48555406+mazurksenija@users.noreply.github.com>
2019-04-12 13:51:00 +0300

    Update lab1.c

--- mazurksenija/lab1.c
+++ mazurksenija/lab1.c
@@ -3,37 +3,92 @@
 
 int main()
 {
-	  int k,l,n,t=0,i;
+	int t=0,i,j,h,s,p,f=0,m=0,g=0,d=1,r,q;
+	float k,l,o,y,n;
 	  printf("Input k:");
-	  scanf("%d", &k);
+	  scanf("%f", &k);
 	  printf("Input l:");
-	  scanf("%d", &l);
-	  n=k;
-	   while(k>0)
+	  scanf("%f", &l);
+	   s=k; p=l;
+		 n=k/l;
+	  if (s%p==0)
+	  {  while(k>=1)
 	   {
-		 k=k/10;
+		 k=k/10.0;
 		 t++;
 	   }
-	  if(k%l==0)
-	  {
-		 printf("  %d|%d",n,l);
-		 printf("\n -%d|---",n);
-		 printf("\n  ",n);
 
-		  for(i=0;i<t;i++)
+		printf("  %d|%d\n",s,p);
+		 o=n*10;
+		 while(n>=1)
 		{
-		  printf("-");
+		  n=n/10;
+		  f++;
+		}
+		n=n*10;
+			 for (i=0;n!=o;i++)
+		{
+			  h=n;
+			  j=(h-g*m)*l;
+			  n=n*10;
+			  m=10;
+			  g=n/m;
+			  y=j;
+		 if (i==0)
+		{  while(y>=1)
+			{
+		 y=y/10.0;
+		 q++;
+		 } printf(" -%d",j);
+		 for(j=0;j<t-q;j++)
+		 {	printf(" ");}
+		 printf("|----\n"); }
+		  if (i==1)
+		   { for(r=0;r<d;r++)
+				{ printf(" ");   }
+			  printf("  %d  %d\n",j,s/p);
+			  for(r=0;r<d;r++)
+			   {  printf(" "); }
+			   printf(" -%d\n",j);
+				d++;   }
+		  if(i>1)
+		  {for(r=0;r<d;r++)
+			   { printf(" ");   }
+			printf("  %d\n",j);
+		   for(r=0;r<d;r++)
+			{	printf(" "); }
+			printf(" -%d\n",j);
+			 d++;     }     }
+			////////////////////////////////////
+		  if (i==1)
+		{ printf("  ");
+		 for(i=0;i<t;i++)
+		   {
+		   printf("-");
+		   }
+			  //printf("-");
+			printf(" %d\n",s/p);
+		  for(i=0;i<t;i++)
+		   {
+		   printf(" ");
+		   }
+		   printf(" 0");
 		}
 
-		  printf(" %d",n/l);
-		  printf("\n");
-
+		 printf("  ");
+		  //////////////////////////////
+			  if (i>=2)
+		{  for(i=0;i<t-1;i++)
+		{
+		   printf("-");
+		}
+			 printf("-\n");  // printf(" %d\n",s/p);
 		  for(i=0;i<t;i++)
 		{
 		   printf(" ");
 		}
-		   printf(" 0");
-	  }
+		   printf(" 0");     }
+								   }
 	  else     printf("Error");
 	  getch();
 	  return 0;
