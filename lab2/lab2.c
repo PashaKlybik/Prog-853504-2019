0cde02384f958ca6f787fa11062c59004dd2dcf1
Катя Симанович <48224454+smnwch@users.noreply.github.com>
2019-05-25 13:51:16 +0300

    Update lab2.c

--- lab2/lab2.c
+++ lab2/lab2.c
@@ -4,16 +4,12 @@
 #include <string.h>
 
 void Information(void) {
-	char str[20];
-	char str1[20];
-	char str2[20];
+	char str[20], str1[20], str2[20];
 	int h = 0;
-	float window = 0, balcony = 0, floor = 0;
+	float window = 0, balcony = 0, floor = 0, i = 0;
 	int costw = 0, costb = 0;
-	float i = 0;
 	printf("Input the number of windows: ");
 	fgets(str,20,stdin);
-	
 	window = atoi(str);
 	if (window < 0 || window != (int)window) {
 		printf("Error");
@@ -77,16 +73,12 @@ void TryAgain(char answer)
 int main()
 {
 	char answer;
-	Information();
-	printf("\n\nDo you want to try again?(y or n): ");
-	scanf(" %c", &answer);
-	TryAgain(answer);
-	while (answer=='y')
-	{
+	do {
 		Information();
-		printf("\nDo you want to try again?(y or n): ");
+		printf("\n\nDo you want to try again?(y or n): ");
 		scanf(" %c", &answer);
 		TryAgain(answer);
 	}
+	while (answer == 'y');
 	return 0;
 }
