ae503a15979b3899f30a9bb78eeadcfe32fb2e56
Катя Симанович <48224454+smnwch@users.noreply.github.com>
2019-05-25 13:37:39 +0300

    Update lab2.c

--- lab2/lab2.c
+++ lab2/lab2.c
@@ -1,22 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <conio.h>
-#include<ctype.h>
+#include <ctype.h>
 #include <string.h>
 
 void Information(void) {
-	char str[20], str1[20], str2[20];
-	float window = 0, balcony = 0, floor = 0, i=0;
-	int costw = 0, costb = 0, h=0;
+	char str[20];
+	char str1[20];
+	char str2[20];
+	int h = 0;
+	float window = 0, balcony = 0, floor = 0;
+	int costw = 0, costb = 0;
+	float i = 0;
 	printf("Input the number of windows: ");
 	fgets(str,20,stdin);
-	while (str[h]) {
-		if (isalpha(str[h])) {
-			printf("error");
-			exit(0);
-		}
-		h++;
-	}
+	
 	window = atoi(str);
 	if (window < 0 || window != (int)window) {
 		printf("Error");
@@ -44,7 +41,7 @@ void Information(void) {
 	fgets(str2,20,stdin);
 	while (str2[h]) {
 		if (isalpha(str2[h])) {
-			printf("error");
+			printf("Error");
 			exit(0);
 		}
 		h++;
@@ -55,36 +52,38 @@ void Information(void) {
 		exit(0);
 	}
 	i = (floor <= 1 ? 0 : costw * 0.15 + costb * 0.15);
-	printf("\nTotal area: %.3f square metres\n",
-		2.15*1.5*window + 0.7*2.15*balcony);
+	printf("\nTotal area: %.3f square metres\n", 2.15*1.5*window + 0.7*2.15*balcony);
 	printf("Cost of windows: %u$\n", costw);
 	printf("Cost of balconies: %u$\n", costb);
 	printf("Surcharge: %.1f$\n", i);
 	printf("Total cost: %.1f$\n", i + costw + costb);
 	printf("Information about us\nCompany: New-Design\nAdress: Minsk, Mazurova, 1, second floor\nPhone: +375296801013\n");
 }
+void TryAgain(char answer)
+{
 
-void TryAgain(char answer) {
-
-	if (answer == 'n') {
+	if (answer=='n')
+	{
 		printf("Goodbye!\n");
-		getch();
 	}
-	else if (answer != 'y') {
+	else if (answer!='y')
+	{
 		printf("Wrong input!\nDo you want to try again?(y or n): ");
 		scanf(" %c", &answer);
 		TryAgain(answer);
 	}
 }
 
-int main() {
+int main()
+{
 	char answer;
-	Information( );
+	Information();
 	printf("\n\nDo you want to try again?(y or n): ");
 	scanf(" %c", &answer);
 	TryAgain(answer);
-	while (answer == 'y') {
-		Information( );
+	while (answer=='y')
+	{
+		Information();
 		printf("\nDo you want to try again?(y or n): ");
 		scanf(" %c", &answer);
 		TryAgain(answer);
