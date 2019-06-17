ed2cd31f965ca91a1f0d9525970b94767d7e00a3
Sheppetamen <48711632+Sheppetamen@users.noreply.github.com>
2019-06-17 19:48:51 +0300

    Update lab2.c

--- Ivan Gerus/lab2.c
+++ Ivan Gerus/lab2.c
@@ -12,23 +12,22 @@
 
 #include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <math.h>
 
 int ax, bx, cx, ay, by, cy;
 double a, b, c;
 double P, S;
-double e = 0.001;
 bool check = false;
+double e = 0.001;
 
 void input()
 {
-    printf("input coordinates\n");
-    scanf("%d", &ax);
-    scanf("%d", &ay);
-    scanf("%d", &bx);
-    scanf("%d", &by);
-    scanf("%d", &cx);
-    scanf("%d", &cy);
+    printf("Input coordinates\n");
+    if(scanf("%d", &ax) != 1 || scanf("%d", &ay) != 1 || scanf("%d", &bx) != 1 || scanf("%d", &by) != 1 || scanf("%d", &cx) != 1 || scanf("%d", &cy) != 1)
+    {
+        exit(1);
+    }
 
     a = powl(pow(ax - bx, 2) + pow(ay - by, 2),0.5);
     b = powl(pow(ax - cx, 2) + pow(ay - cy, 2),0.5);
@@ -37,7 +36,22 @@ void input()
 
 void type()
 {
-    if ((pow(a, 2)-pow(b, 2)-pow(c, 2))<=e || (pow(b, 2)-pow(c, 2)-pow(a, 2))<=e ||(pow(c, 2)-pow(b, 2)-pow(a, 2))<=e)
+    if(a == 0 || b == 0 || c == 0)
+    {
+        printf("It's line or dot");
+        return;
+    }
+    if ((pow(a, 2)-pow(b, 2)-pow(c, 2))<=e && (pow(a, 2)-pow(b, 2)-pow(c, 2))>=0)
+    {
+        check = true;
+        printf("Прямоугольный\n");
+    }
+    if ((pow(b, 2)-pow(a, 2)-pow(c, 2))<=e && (pow(b, 2)-pow(a, 2)-pow(c, 2))>=0)
+    {
+        check = true;
+        printf("Прямоугольный\n");
+    }
+    if ((pow(c, 2)-pow(b, 2)-pow(a, 2))<=e && (pow(c, 2)-pow(b, 2)-pow(a, 2))>=0)
     {
         check = true;
         printf("Прямоугольный\n");
@@ -49,17 +63,13 @@ void type()
         {
             printf("Равносторонний\n");
         }
-            printf("Равнобедренный\n");
+        printf("Равнобедренный\n");
         
     }
-    
-
     if(check == false)
     {
         printf("Произвольный\n");
     }
-    
-
 }
 
 double perimetr()
@@ -91,36 +101,40 @@ int main()
     while (l != 0)
     {
         printf("1 - input\n2 - type\n3 - perimeter\n4 - area\n5 - radius of inscribed and outscribed circles\n6 - info about version and author\n7 - exit\n");
-        scanf("%d", &l);
-        if (l == 1)
+        
+        if(scanf("%d", &l) == 1 && (l>=1 && l<=7))
         {
-            input();
-        }
-        if (l == 2)
-        {
-            type();
-        }
-        if (l == 3)
-        {
-            printf("");
-            printf("%f\n", perimetr());
-        }
-        if (l == 4)
-        {
-            printf("");
-            printf("%f\n", area());
-        }
-        if (l == 5)
-        {
-            circles();
-        }
-        if (l == 6)
-        {
-            printf("v.1\nGIA\n");
-        }
-        if (l ==7)
-        {
-            return 0;
+            if (l == 1)
+            {
+                input();
+            }
+            if (l == 2)
+            {
+                type();
+            }
+            if (l == 3)
+            {
+                printf("");
+                printf("%f\n", perimetr());
+            }
+            if (l == 4)
+            {
+                printf("");
+                printf("%f\n", area());
+            }
+            if (l == 5)
+            {
+                circles();
+            }
+            if (l == 6)
+            {
+                printf("v.1\nGIA\n");
+            }
+            if (l ==7)
+            {
+                return 0;
+            }
         }
     }
+    return 0;
 }
